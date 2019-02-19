f9ae6ab2f2d44abb7ab7fc8ad53c65977250b9b5
ricardoalves1 <ras@ic.ufal.br>
2019-02-19 17:20:42 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 // DB
-define('DB_DRIVER', 'postgreSQL');
+define('DB_DRIVER', 'psgsql');
 define('DB_HOSTNAME', 'ec2-79-125-4-96.eu-west-1.compute.amazonaws.com');
 define('DB_USERNAME', 'kddxbwfweamtzp');
 define('DB_PASSWORD', '00bc70779919747cdab4d079e89884d3c16758c924a711b3044a8109568daff5');
