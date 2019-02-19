1178283386a5c9db98c6012063d8b3133ce0420f
ricardoalves1 <ras@ic.ufal.br>
2019-02-19 17:21:35 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,1 @@
-// DB
-define('DB_DRIVER', 'psgsql');
-define('DB_HOSTNAME', 'ec2-79-125-4-96.eu-west-1.compute.amazonaws.com');
-define('DB_USERNAME', 'kddxbwfweamtzp');
-define('DB_PASSWORD', '00bc70779919747cdab4d079e89884d3c16758c924a711b3044a8109568daff5');
-define('DB_DATABASE', 'd9ssv6ots98h13');
-define('DB_PORT', '5432');
-define('DB_PREFIX', 'oc_');
+
