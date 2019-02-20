e7534cc87f01e77caa8c87ccdd456d87d958b05c
ricardoalves1 <ras@ic.ufal.br>
2019-02-20 12:34:50 -0300

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ define('HTTP_SERVER', 'https://websiteauto.herokuapp.com/');
 // HTTPS
 define('HTTPS_SERVER', 'https://websiteauto.herokuapp.com/');
 // DIR
-define('DIR_APPLICATION', '/app/storage/catalog/');
-define('DIR_SYSTEM', '/app/storage/system/');
-define('DIR_IMAGE', '/app/storage/image/');
-define('DIR_STORAGE', '/app/storage/storage/');
+define('DIR_APPLICATION', '/app/catalog/');
+define('DIR_SYSTEM', '/app/system/');
+define('DIR_IMAGE', '/app/image/');
+define('DIR_STORAGE', '/app/storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
